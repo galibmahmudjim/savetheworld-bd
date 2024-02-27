02ac529c65967ad075a9f819e23ab70fb43e5072
Galib Mahmud Jim <galibmahmudjim@gmail.com>
2024-02-27 12:09:43 +0600

    new navtab

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Headers from "../components/header";
 import NavBar from "../components/nav";
 import TabBar from "../components/tab";
 import Image from "next/image";
-import NavTab from "@/components/navtab";
+import NavTab from "../components/navtab";
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,700&display=swap')
   @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap')
