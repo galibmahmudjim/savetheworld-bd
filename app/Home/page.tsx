8be810397a6a1e31ad4005ba3596d11b2140e90e
Galib Mahmud Jim <galibmahmudjim@gmail.com>
2024-02-27 14:20:04 +0600

    update

---
+++ app/Home/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import "./home.css";
+const Home: React.FC = () => {
+
+      return (
+            <div className="w-full flex flex-col justify-center items-center">
+
+            </div>
+      );
+};
+
+export default Home;
