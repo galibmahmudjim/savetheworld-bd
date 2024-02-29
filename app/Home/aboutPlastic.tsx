378b02455f625188b55852d1ade142dc7c5aa39b
Galib Mahmud Jim <galibmahmudjim@gmail.com>
2024-02-29 21:46:43 +0600

    New Home page added about plastic updated more

--- app/Home/aboutPlastic.tsx
+++ app/Home/aboutPlastic.tsx
@@ -6,16 +6,16 @@ const AboutPlastic = () => {
                   <h4 className="subsubsection"> Plastic is mainly a petroleum-derived non-biodegradable polymer. Today modern life is almost impossible without plastics
                   </h4>
                   <ul className="ulHome">
-                        <li className="checkmark">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} />
+                        <li className="checkmarks">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} />
                               <p className="w-[90%]">It is estimated that around 343 million tonnes (MT) of plastic is produced every year.</p>
                         </li>
-                        <li className="checkmark">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} />
+                        <li className="checkmarks">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} />
                               <p className="w-[90%]">Plastic Wastes (PW) are produced by massive manufacturing, processing, and packaging industries.</p></li>
-                        <li className="checkmark">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} />
+                        <li className="checkmarks">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} />
                               <p className="w-[90%]">More than 20 billion plastic bottles are tossed on earth.</p>
                         </li>
-                        <li className="checkmark">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} /><p className="w-[90%]">Only approximately 10% of total world plastic is recycled.</p></li>
-                        <li className="checkmark">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} /><p className="w-[90%]">Plastic visibility is increasing due to plastic accumulation and its harmful influence on the environment and human health.</p>
+                        <li className="checkmarks">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} /><p className="w-[90%]">Only approximately 10% of total world plastic is recycled.</p></li>
+                        <li className="checkmarks">  <HiOutlineClipboardDocumentList color="#3565BE" size={25} /><p className="w-[90%]">Plastic visibility is increasing due to plastic accumulation and its harmful influence on the environment and human health.</p>
                         </li>
                   </ul>
             </div>
