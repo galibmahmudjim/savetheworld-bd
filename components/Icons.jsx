e0488fe5035c0a8f7ab66c0231f25a3d19e45b99
Galib Mahmud Jim <galibmahmudjim@gmail.com>
2024-07-13 02:59:51 +0600

    modify

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -5,52 +5,7 @@ export const ChevronDown = ({fill, size, height, width, ...props}) => {
       fill="none"
       height={size || height || 24}
       viewBox="0 0 24 24"
-      width={size || width || 24}
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
-        stroke={fill}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeMiterlimit={10}
-        strokeWidth={1.5}
-      />
-    </svg>
-  );
-};
-
-export const Lock = ({fill, size, height, width, ...props}) => {
-  const color = fill;
-
-  return (
-    <svg
-      height={size || height || 24}
-      viewBox="0 0 24 24"
-      width={size || width || 24}
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <g transform="translate(3.5 2)">
-        <path
-          d="M9.121,6.653V4.5A4.561,4.561,0,0,0,0,4.484V6.653"
-          fill="none"
-          stroke={color}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeMiterlimit="10"
-          strokeWidth={1.5}
-          transform="translate(3.85 0.75)"
-        />
-        <path
-          d="M.5,0V2.221"
-          fill="none"
-          stroke={color}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeMiterlimit="10"
-          strokeWidth={1.5}
+      asdas
           transform="translate(7.91 12.156)"
         />
         <path
